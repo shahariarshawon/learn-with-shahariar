1592e1dea15c623ab4b6c9d0b8f361f8be7efab0
shahariarshawon <shahariararafat7739@gmail.com>
2026-03-17 11:54:34 +0600

    Login page has been redesgined by fixing width problems and responsiveness

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,34 +1,55 @@
-import React from "react";
-import loginImage from "../assets/login.png";
+import React, { useState } from "react";
+import loginImage from "../assets/login.jfif";
+import { FiEye, FiEyeOff } from "react-icons/fi";
 
 const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [activeTab, setActiveTab] = useState("login");
+
   return (
-    <div className="hero bg-base-200 min-h-screen px-6">
-      <div className="hero-content flex-col lg:flex-row gap-12 max-w-6xl items-stretch">
+    <div className="flex justify-center items-center bg-base-200 min-h-screen px-6">
+      <div className="flex flex-col lg:flex-row gap-10 max-w-6xl w-full bg-white md:p-15 rounded-2xl shadow-lg">
 
         {/* LEFT IMAGE */}
-        <div className="flex-1">
+        <div className="hidden lg:block w-3/4">
           <img
             src={loginImage}
             alt="login"
-            className="w-full h-full object-cover rounded-2xl shadow-xl"
+            className="w-full h-full object-cover rounded-2xl"
           />
         </div>
 
         {/* RIGHT FORM */}
-        <div className="flex-1  p-10 rounded-2xl ">
+        <div className="w-full p-10 lg:w-1/2">
 
           <h1 className="text-4xl font-bold text-center mb-6">
             Welcome Back!
           </h1>
 
-          <div className="flex justify-between bg-[#49bbbd] rounded-full p-1 mb-8">
-            <p className="bg-[#245151] text-white rounded-full px-10 py-2 font-medium">
+          {/* TOGGLE */}
+          <div className="relative flex bg-[#49bbbd] rounded-full p-1 mb-8 cursor-pointer">
+
+            {/* sliding background */}
+            <div
+              className={`absolute top-1 bottom-1 w-1/2 bg-[#245151] rounded-full transition-all duration-300 ${
+                activeTab === "login" ? "left-1" : "left-[50%]"
+              }`}
+            ></div>
+
+            <p
+              onClick={() => setActiveTab("login")}
+              className="flex-1 text-center z-10 text-white py-2 font-medium"
+            >
               Login
             </p>
-            <p className="text-white px-10 py-2 font-medium">
+
+            <p
+              onClick={() => setActiveTab("signup")}
+              className="flex-1 text-center z-10 text-white py-2 font-medium"
+            >
               Sign Up
             </p>
+
           </div>
 
           <p className="text-gray-500 text-center mb-8">
@@ -46,13 +67,24 @@ const LoginPage = () => {
           </div>
 
           {/* PASSWORD */}
-          <div className="mb-5">
+          <div className="mb-5 relative">
             <h3 className="mb-2 font-medium">Password</h3>
+
             <input
-              type="password"
+              type={showPassword ? "text" : "password"}
               placeholder="Enter your password"
-              className="w-full border border-gray-300 rounded-lg px-4 py-3 focus:outline-none focus:border-[#49bbbd]"
+              className="w-full border border-gray-300 rounded-lg px-4 py-3 pr-12 focus:outline-none focus:border-[#49bbbd]"
             />
+
+            {/* SHOW BUTTON */}
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-3 top-12 text-sm text-gray-500 hover:text-[#49bbbd] hover:cursor-pointer"
+            >
+              {showPassword ? <FiEyeOff></FiEyeOff> : <FiEye></FiEye>}
+            </button>
+
           </div>
 
           {/* REMEMBER */}
@@ -69,7 +101,7 @@ const LoginPage = () => {
 
           {/* BUTTON */}
           <button className="w-full py-3 rounded-lg bg-[#49bbbd] text-white font-semibold hover:opacity-90 transition">
-            Login
+            {activeTab === "login" ? "Login" : "Sign Up"}
           </button>
 
         </div>
